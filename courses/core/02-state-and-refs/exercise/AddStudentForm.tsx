4d22f7cec622af1ac35eb3e48c2a599d47ac6f87
Dmitry Nortsev <dmitry.nortsev@gmail.com>
2022-03-28 18:50:58 -0500

    Add corse 2 implementation
    Signed-off-by: Dmitry Nortsev <dmitry.nortsev@gmail.com>

--- courses/core/02-state-and-refs/exercise/AddStudentForm.tsx
+++ courses/core/02-state-and-refs/exercise/AddStudentForm.tsx
@@ -1,17 +1,26 @@
 import { useState, useRef } from 'react'
 import { Heading } from 'course-platform/Heading'
 
+const initialFormValues = {
+  fullName: '',
+  username: '',
+}
+
 export function AddStudentForm() {
-  const [fullName, setFullName] = useState('')
-  const [username, setUsername] = useState('')
+  const [formValues, setFormValues] = useState(initialFormValues)
   const [autoUsername, setAutoUsername] = useState(true)
+
+  const fullNameRef = useRef<HTMLInputElement>(null!)
+
+  function setField(field: string, value: string) {
+    setFormValues({ ...formValues, [field]: value })
+  }
 
   function handleSubmit(event: React.FormEvent) {
     event.preventDefault()
-    console.log({
-      fullName,
-      username,
-    })
+    console.log(formValues)
+    setFormValues(initialFormValues)
+    fullNameRef.current.focus()
   }
 
   return (
@@ -19,24 +28,48 @@ export function AddStudentForm() {
       <Heading>Add Student</Heading>
       <div className="field-wrap">
         <label htmlFor="full-name">Full Name</label>
-        <input id="full-name" type="text" className="form-field" required />
+        <input
+          ref={fullNameRef}
+          id="full-name"
+          type="text"
+          className="form-field"
+          value={formValues.fullName}
+          onChange={(e) => setField('fullName', e.target.value)}
+          required
+        />
       </div>
 
       <div className="field-wrap">
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" className="form-field" required autoComplete="off" />
+        <input
+          value={
+            autoUsername
+              ? formValues.fullName.toLowerCase().replaceAll(/\s/g, '')
+              : formValues.username
+          }
+          id="username"
+          type="text"
+          className="form-field"
+          onChange={(e) => setField('username', e.target.value)}
+          required
+          autoComplete="off"
+        />
       </div>
 
       <div>
         <label className="vertical-middle horizontal-spacing">
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={autoUsername}
+            onChange={(e) => setAutoUsername(!autoUsername)}
+          />
           <span>Auto Username</span>
         </label>
       </div>
 
       <hr />
       <button type="submit" className="button">
-        Add Student
+        s Add Student
       </button>
     </form>
   )
